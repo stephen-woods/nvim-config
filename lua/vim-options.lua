dbf8b5c420a8384c87eac9a77a47aa485f1e5478
Stephen Woods <stephen_woods@cable.comcast.com>
2024-01-16 22:06:33 -0500

    Add nvim-tree

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,11 +5,13 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>',{})
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>',{})
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>',{})
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>',{})
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', {})
 vim.wo.number = true
 
+-- NVimTree
+vim.keymap.set('n', '<C-n>', '<cmd> NvimTreeToggle <CR>', {})
