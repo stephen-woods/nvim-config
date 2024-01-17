c685291f889204c8ec6a2878a3892218871da003
Stephen Woods <stephen_woods@cable.comcast.com>
2024-01-17 18:05:17 -0500

    Added lualine. Additional key mappings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set rnu")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
@@ -16,3 +17,22 @@ vim.wo.number = true
 -- NVimTree
 vim.keymap.set('n', '<C-n>', '<cmd> NvimTreeToggle <CR>', {})
 
+-- Remap page forard/backward to center cursor in the middle of the screen
+vim.keymap.set({'n','v'},'<C-f>','<C-f>zz')
+vim.keymap.set({'n','v'},'<C-b>','<C-b>zz')
+vim.keymap.set({'n','v'},'<C-d>','<C-d>zz')
+vim.keymap.set({'n','v'},'<C-u>','<C-u>zz')
+
+vim.keymap.set('n', '<leader>n', '<cmd> set nu! <CR>')
+vim.keymap.set('n', '<leader>rn', '<cmd> set rnu! <CR>')
+
+vim.keymap.set('n', '<leader>ff', '<cmd> Telescope find_files <CR>')
+vim.keymap.set('n', '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>')
+vim.keymap.set('n', '<leader>fw', '<cmd> Telescope live_grep <CR>')
+vim.keymap.set('n', '<leader>fb', '<cmd> Telescope buffers <CR>')
+vim.keymap.set('n', '<leader>fo', '<cmd> Telescope oldfiles <CR>')
+vim.keymap.set('n', '<leader>fz', '<cmd> Telescope current_buffer_fuzzy_find <CR>')
+
+vim.keymap.set('n', '<leader>cm', '<cmd> Telescope git_commits <CR>')
+vim.keymap.set('n', '<leader>gt', '<cmd> Telescope git_status <CR>')
+vim.keymap.set('n', '<leader>ma', '<cmd> Telescope marks <CR>')
