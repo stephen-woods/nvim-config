279d86bfa617b462f11f4cc370dfce2cd55b5e58
Stephen Woods <stephen_woods@cable.comcast.com>
2024-01-20 15:15:34 -0500

    Add trouble diagnostics

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -62,10 +62,13 @@ vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 
-
-
-
-
+-- Trouble
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
 
 
 vim.keymap.set('v', '<', '<gv')
