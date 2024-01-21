130efeb2f33749e1cd906d5e8104fad6a82f7d3e
Stephen Woods <stephen.woods@altarasystems.com>
2024-01-21 10:55:24 -0500

    Setup Scala Metals

---
+++ lua/plugins/dap-config.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-lua/plenary.nvim",
+  {
+    "mfussenegger/nvim-dap",
+    config = function(self, opts)
+      -- Debug settings if you're using nvim-dap
+      local dap = require("dap")
+
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
+    end
+  },
+}
