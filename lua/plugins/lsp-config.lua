070a298eb82121980871d4116192988229a5c729
Stephen Woods <stephen.woods@altarasystems.com>
2024-02-13 21:28:26 -0500

    Add lsp mapping descriptions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -54,7 +54,7 @@ return {
     "scalameta/nvim-metals",
     ft = {"scala", "sbt", "java"},
     dependencies = "nvim-lua/plenary.nvim",
-        opts = function()
+    opts = function()
       local metals_config = require("metals").bare_config()
 
       -- Example of settings
@@ -81,17 +81,17 @@ return {
 
         -- LSP mappings
         local map = vim.keymap.set
-        map("n", "gD", vim.lsp.buf.definition)
-        map("n", "K", vim.lsp.buf.hover)
-        map("n", "gi", vim.lsp.buf.implementation)
-        map("n", "gr", vim.lsp.buf.references)
-        map("n", "gds", vim.lsp.buf.document_symbol)
-        map("n", "gws", vim.lsp.buf.workspace_symbol)
-        map("n", "<leader>cl", vim.lsp.codelens.run)
-        map("n", "<leader>sh", vim.lsp.buf.signature_help)
-        map("n", "<leader>rn", vim.lsp.buf.rename)
-        map("n", "<leader>f", vim.lsp.buf.format)
-        map("n", "<leader>ca", vim.lsp.buf.code_action)
+        map("n", "gD", vim.lsp.buf.definition, { desc = "LSP jump to symbol definition" })
+        map("n", "K", vim.lsp.buf.hover, { desc = "LSP display symbol information" })
+        map("n", "gi", vim.lsp.buf.implementation, { desc = "LSP list all implementations" })
+        map("n", "gr", vim.lsp.buf.references, { desc = "LSP list all references" })
+        map("n", "gds", vim.lsp.buf.document_symbol, { desc = "LSP display all symbols in buffer" })
+        map("n", "gws", vim.lsp.buf.workspace_symbol, { desc = "LSP display all symbols in workspace" })
+        map("n", "<leader>cl", vim.lsp.codelens.run, { desc = "LSP run code lens in current line" })
+        map("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "LSP display signature help" })
+        map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename all references to symbol" })
+        map("n", "<leader>f", vim.lsp.buf.format, { desc = "LSP format buffer" })
+        map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
 
         map("n", "<leader>ws", function()
           require("metals").hover_worksheet()
@@ -151,11 +151,10 @@ return {
           require("dap").run_last()
         end)
 
-        map("n", "<leader>mc", function ()
+        map("n", "<leader>mc", function()
           require("telescope").extensions.metals.commands()
         end)
       end
-
       return metals_config
     end,
     config = function(self, metals_config)
