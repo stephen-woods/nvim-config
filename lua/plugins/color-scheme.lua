a17aa2f72cdfce46e499b59e3066461f7c7a5ff9
Stephen Woods <stephen.woods@altarasystems.com>
2024-02-25 12:14:31 -0500

    Catppuccin - need to recompile Catppuccin for changes to take effect

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      vim.cmd [[colorscheme tokyonight-night]]
+    end
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    flavour = "mocha",
+    priority = 1000,
+    opts = {
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+        which_key = false,
+        dap = {
+          enabled = false,
+          enable_ui = false,
+        }
+      }
+    },
+    init = function()
+      vim.cmd [[colorscheme catppuccin]]
+    end
+  },
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "tokyonight",
+        -- theme = "tokyonight",
+        theme = "catppuccin",
       },
     })
   end,
