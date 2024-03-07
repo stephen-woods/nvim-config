0094a0917f0d08cff79e6d86ba0f0497c8da9c1c
Stephen Woods <stephen_woods@cable.comcast.com>
2024-03-06 21:27:37 -0500

    Updates and cmp-lua

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,22 +1,12 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
-    "hrsh7th/cmp-path"
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "L3MON4D3/LuaSnip",
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
       local cmp = require("cmp")
