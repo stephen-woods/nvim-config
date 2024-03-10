0892e6ac9e838324930606f97b877f23b819a49d
Stephen Woods <stephen_woods@cable.comcast.com>
2024-03-10 17:23:50 -0400

    Upgrade plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -100,6 +100,8 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzy find in current buffer' })
     end,
   },
   {
