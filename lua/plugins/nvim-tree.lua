c702f508e544c08d72e8edb176c4256d191e0d48
Stephen Woods <stephen_woods@cable.comcast.com>
2024-05-24 14:52:47 -0400

    Adaptive width for nvim tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -18,7 +18,7 @@ return {
           update_root = false,
         },
         view = {
-          adaptive_size = false,
+          adaptive_size = true,
           side = "left",
           width = 30,
           preserve_window_proportions = true,
