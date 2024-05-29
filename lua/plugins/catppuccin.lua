933e1f2ee2d0b1bccb1a90c767d949e13dd3714d
Stephen Woods <stephen_woods@cable.comcast.com>
2024-05-28 22:05:37 -0400

    Added BadSpell highlighting to catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -22,9 +22,13 @@ return {
         all = function(color)
           return {
             LineNr = { fg = color.flamingo },
+            SpellBad = { style = { "undercurl" }, sp = color.red, },
+            SpellCap = { style = { "undercurl"}, sp = color.red, },
+            SpellLocal = { style = { "undercurl" }, sp = color.red, },
+            SpellRare = { style = { "undercurl" }, sp = color.red, },
           }
         end,
-      }
+      },
     },
     init = function()
       vim.o.termguicolors = true
